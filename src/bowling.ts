79350e8da0c951a6d56b3d485f5e64039b6a0b5c
Chris Rhodes <crr0004@gmail.com>
2021-11-08 15:54:41 +1100

    Introduce basic state machine and get test working for basic rolls

--- src/bowling.ts
+++ src/bowling.ts
@@ -1,9 +1,12 @@
 import { InvalidSpareFrame, InvalidStrikeFrame } from "./errors";
 
 const MAX_FRAMES = 10;
-const PINS = 10;
+const MAX_PINS = 10;
+enum FrameStatus {
+    UNROLLED
+}
 class Frame{
-    rolls: [number, number];
+    rolls: [number | FrameStatus, number | FrameStatus];
     constructor(rolls: [number, number]){
         this.rolls = rolls;
     }
@@ -27,14 +30,61 @@ export class StrikeFrame extends Frame{
     }
 }
 
-export class Bowling{
+interface State{
+    readonly frame: Frame;
+    readonly nextFunction: (pins: number, currentFrame: State) => State;
+}
 
-    scores = Array<Number>(MAX_FRAMES);
+function newFrame(pins: number, currentFrame: State): State{
+    return {
+        frame: new Frame([pins, FrameStatus.UNROLLED]),
+        nextFunction: score
+    };
+
+}
+function score(pins: number, currentState: State): State{
+    return {
+        frame: new Frame([currentState.frame.rolls[0], pins]),
+        nextFunction: newFrame
+    }
+}
+
+function spare(pins: number): State{
+    return {
+        frame: new Frame([FrameStatus.UNROLLED, FrameStatus.UNROLLED]),
+        nextFunction: newFrame
+    };
+
+}
+
+function strike(pins: number): State{
+    return {
+        frame: new Frame([FrameStatus.UNROLLED, FrameStatus.UNROLLED]),
+        nextFunction: newFrame
+    };
+}
+
+export class Bowling{
+    
+    scores = Array<State>();
+    state: State = {
+        frame: new Frame([FrameStatus.UNROLLED, FrameStatus.UNROLLED]),
+        nextFunction: newFrame
+    };
     roll(pins: number) {
-        
+        const nextState = this.state.nextFunction(pins, this.state);
+        // Only want to store the state when both rolls have been made
+        if(nextState.frame.rolls[0] != FrameStatus.UNROLLED && nextState.frame.rolls[1] != FrameStatus.UNROLLED){
+            this.scores.push(nextState);
+        }
+        this.state = nextState;
     }
 
     score(): number{
-        return 0;
+        let score = 0;
+        this.scores.forEach((state) => {
+            score += (state.frame.rolls[0] + state.frame.rolls[1]);
+        })
+        return score;
     }
 }
